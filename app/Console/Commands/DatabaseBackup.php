f5a3fd52f55fafaffb75130459b0a5bb9e89b60d
Rafael Burgueño <rafaelburg@gmail.com>
2023-07-17 21:50:10 -0300

    arregle el problema de los backups diarios que se creaban vacios

--- app/Console/Commands/DatabaseBackup.php
+++ app/Console/Commands/DatabaseBackup.php
@@ -3,6 +3,8 @@
 namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Mail;
+use App\Mail\EmailDeControl;
 
 class DatabaseBackup extends Command
 {
@@ -30,9 +32,35 @@ class DatabaseBackup extends Command
 
         $fecha = date('Y-m-d_H-i-s');
         $nombreArchivo = 'backup_'.$fecha.'.sql';
-        $comando = 'mysqldump -u '.env('DB_USERNAME').' -p'.env('DB_PASSWORD').' '.env('DB_DATABASE').' > '.$nombreArchivo;
-        exec($comando);
-        $this->info('Backup de la base de datos realizado con éxito');
+        $comando = '/usr/bin/mysqldump -u '.env('DB_USERNAME').' -p"'.env('DB_PASSWORD').'" '.env('DB_DATABASE').' > '.$nombreArchivo;
+        
+        //exec($comando);
+        exec($comando, $output, $return_var);
+
+        // envia un email con $output
+        $data = [
+            'output' => $output,
+            'return_var' => $return_var
+        ];
+        /*Mail::send('emails.backup', $data, function($message) use ($data) {
+            $message->to('')->subject('Backup de la base de datos');
+        });*/
+        //Mail::to(env('MAIL_REGISTROS', 'rafaelburg@gmail.com'))->queue(new EmailDeControl($data));
+
+
+        // Imprime la salida en la consola
+        foreach ($output as $line) {
+            $this->info($line);
+        }
+
+        // Verifica si hay errores
+        if ($return_var !== 0) {
+            $this->error('Ocurrió un error al ejecutar el comando mysqldump');
+        } else {
+            $this->info('Backup de la base de datos realizado con éxito');
+        }
+
+        //$this->info('Backup de la base de datos realizado con éxito');
 
         // envia el archivo al directorio de backups
         $comando = 'mv '.$nombreArchivo.' ../database-backups/';
